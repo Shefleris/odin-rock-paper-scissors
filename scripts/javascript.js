9dcccbf29ce199f829e0c96db6c73b0fbcc2f26e
Sefleris <p.sefleris@gmail.com>
2024-07-22 16:38:23 +0300

    Add function to get random computer choice

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -16,3 +16,10 @@ function getHumanChoice () {
 };
 
 console.log(getHumanChoice());
+
+function getComputerChoice () {
+    let computerChoices = ["rock", "paper", "scissors"];
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
+
+console.log(getComputerChoice());

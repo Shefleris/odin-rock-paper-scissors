1893ce0a0bbf9c05584cf04f46d650430333092e
Sefleris <p.sefleris@gmail.com>
2024-07-22 16:29:48 +0300

    Write the rps pseudocode

---
+++ scripts/javascript.js
@@ -0,0 +1,12 @@
+/* ==Pseudocode==
+    1. Get the human choice
+    2. Get the computer choice
+    3. Compare the choices. If:
+        a. Same choice = show that it's a draw.
+        b. Inferior choice = Show that computer wins.
+        c. Superior choice = Show that you win.
+    4. Increment the winners score
+    5. Increment the round number
+    6. Repeat steps 1 through 5 until the round number equals 5
+    7. Announce the game winner.
+*/
